fed6ca1fc5ec99afc2e91768da9bd1e3caadef1c
johnmcdnl <john.mcdonnell439@gmail.com>
2016-09-15 21:45:33 +0100

    update

--- resources/targets/targetsRouter.go
+++ resources/targets/targetsRouter.go
@@ -20,7 +20,7 @@ func TargetsRouter() chi.Router {
 	r.Get(targetNameUrl, targetNameHandler)
 
 	r.Get(usernameNameUrl, usernameHandler)
-	r.Get(usernameNameUrl + targetNameUrl, usernameTargetNameHandler)
+	r.Get(usernameNameUrl+targetNameUrl, usernameTargetNameHandler)
 
 	return r
 }
@@ -37,29 +37,38 @@ func allTargetsHandler(w http.ResponseWriter, r *http.Request) {
 	targets, err := retrieveAllTargets()
 
 	if err != nil {
-		render.JSON(w, http.StatusNotFound, err.Error())
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, err.Error())
 		return
 	}
 
-	render.JSON(w, http.StatusOK, targets)
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, targets)
 }
 
 func usernameHandler(w http.ResponseWriter, r *http.Request) {
 	targets, err := retrieveAllTargetsByUsername(usernameParam(r))
 	if err != nil {
-		render.JSON(w, http.StatusNotFound, err.Error())
+
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, err.Error())
 		return
 	}
-	render.JSON(w, http.StatusOK, targets)
+
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, targets)
 }
 
 func targetNameHandler(w http.ResponseWriter, r *http.Request) {
 	targets, err := retrieveAllTargetsByTargetName(targetNameParam(r))
 	if err != nil {
-		render.JSON(w, http.StatusNotFound, err.Error())
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, err.Error())
 		return
 	}
-	render.JSON(w, http.StatusOK, targets)
+
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, targets)
 }
 
 func usernameTargetNameHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,24 +76,29 @@ func usernameTargetNameHandler(w http.ResponseWriter, r *http.Request) {
 	targets, err := retrieveAllTargetsByUsernameAndTargetName(usernameParam(r), targetNameParam(r))
 
 	if err != nil {
-		render.JSON(w, http.StatusNotFound, err.Error())
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, err.Error())
 		return
 	}
 
-	render.JSON(w, http.StatusOK, targets)
+	render.Status(r, http.StatusOK)
+	render.JSON(w, r, targets)
 }
 
 func createTargetsMatch(w http.ResponseWriter, r *http.Request) {
 	t, err := bindTargetFromRequest(r)
 	if err != nil {
-		render.JSON(w, http.StatusBadRequest, err.Error())
+		render.Status(r, http.StatusBadRequest)
+		render.JSON(w, r, err.Error())
 		return
 	}
 
 	if t, err = createTarget(t); err != nil {
-		render.JSON(w, http.StatusInternalServerError, err.Error())
+		render.Status(r, http.StatusInternalServerError)
+		render.JSON(w, r, err.Error())
 		return
 	}
 
-	render.JSON(w, http.StatusCreated, t)
+	render.Status(r, http.StatusCreated)
+	render.JSON(w, r, t)
 }
